e4633689faa4e899afc61add6bfa40b3fded5603
JoseBernalRodriguez <62001295+JoseBernalRodriguez@users.noreply.github.com>
2021-04-24 00:03:05 -0500

    Deleted Multiplicaciones

--- Multiplicaciones/multiplicacion_armadillo.cpp
+++
@@ -1,6 +0,0 @@
-#include <armadillo>
-void multplicacion_armadillo(arma::Mat<double> & A,arma::Mat<double>& B,arma::Mat<double> & C)
-{
-  C=2.0*A*B;
-  
-}
--- Multiplicaciones/multiplicacion_eigen.cpp
+++
@@ -1,5 +0,0 @@
-#include <Eigen/Dense>
-void multiplicacion_eigen(const Eigen::MatrixXd & A, const Eigen::MatrixXd & B, const Eigen::MatrixXd & C)
-{
-  C=2.0*A*B;
-}
