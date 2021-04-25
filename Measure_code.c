362b0bcd236ac2edc1b4d6c4b831cfea96441285
JoseBernalRodriguez <62001295+JoseBernalRodriguez@users.noreply.github.com>
2021-04-24 23:41:47 -0500

    constantes y desviación standard añadidos al archivo main

--- Measure_code.c
+++ Measure_code.c
@@ -54,5 +54,17 @@ int main()
   
 }
 
+double desviacion_estandar(std::vector<double> & MFLOPS)
+{
+  double sigma=0;
+  double N=MFLOPS.size();
+  for(int i=1;i<N;i++)
+    {
+      sigma+=std::pow((MFLOPS[i]-MFLOPS[0]),2);
+    }
+  sigma=std::sqrt(sigma/N);
+  return sigma;
+}
+
 
 
