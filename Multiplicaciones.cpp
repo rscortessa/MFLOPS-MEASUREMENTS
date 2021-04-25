69947b3672ac3b569c5913f508deae599f37fb56
JoseBernalRodriguez <62001295+JoseBernalRodriguez@users.noreply.github.com>
2021-04-24 23:09:29 -0500

    mod en mult-blocking

--- Multiplicaciones.cpp
+++ Multiplicaciones.cpp
@@ -22,7 +22,8 @@ void multiplicacion_directa(std::vector<double> & A,std::vector<double> & B,std:
 }
 void multiplicacion_blocking(const std::vector<double> a,const std::vector<double> b,std::vector<double> &c,int Nb)
 {
-    int N=sqrt(a.size())/Nb;
+    int M=sqrt(a.size());
+    int N=M/Nb;
     for(int block_i=1;block_i<N+1;block_i++)
     {
         for(int block_j=1;block_j<N+1;block_j++)
@@ -35,7 +36,7 @@ void multiplicacion_blocking(const std::vector<double> a,const std::vector<doubl
                     {
                         for(int k=(block_k-1)*Nb;i<block_k*Nb;k++)
                         {
-                            c[i,j]+=a[i,k]*b[k,j];
+                            c[i,j]+=a[i*M+k]*b[M*k+j];
                         }
                     }
                 }
