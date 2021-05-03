e59e7b401cd84aad6d67616710d4c1d294b08d01
JoseBernalRodriguez <62001295+JoseBernalRodriguez@users.noreply.github.com>
2021-05-02 20:34:19 -0500

    added ifdefine and erased unnecessary arguments

--- Transpuestas.h
+++ Transpuestas.h
@@ -1,7 +1,10 @@
+#ifndef _TRANSPUESTAS_H_
+#define _TRANSPUESTAS_H_
 #include <Eigen/Dense>
 #include <armadillo>
 #include <vector>
-void transpuesta_directa(std::vector<double> & A, int N);
-void transpuesta_eigen(const Eigen::MatrixXd & A);
+void transpuesta_directa(std::vector<double> & A);
+void transpuesta_eigen(Eigen::MatrixXd & A);
 void transpuesta_armadillo(arma::Mat<double> & A);
-void transpuesta_blocking(std::vector<double> & A, std::vector<double>&ABT, const int N, int P);
+void transpuesta_blocking(std::vector<double> & A, std::vector<double>&ABT, int P)
+#endif
