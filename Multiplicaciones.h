3a5e7652add51ce9c9b540fcc8004f6c34aa041d
Federico-Aranguren <79715183+Federico-Aranguren@users.noreply.github.com>
2021-05-05 22:51:04 -0500

    Syntaxis error

--- Multiplicaciones.h
+++ Multiplicaciones.h
@@ -6,7 +6,7 @@
 #include <vector>
 
 void multiplicacion_directa(const std::vector<double> & A,const std::vector<double> & B,std::vector<double> & C);
-void multiplicacion_eigen(const Eigen::MatrixXd & A, const Eigen::MatrixXd & B, const Eigen::MatrixXd & C);
+void multiplicacion_eigen(const Eigen::MatrixXd & A, const Eigen::MatrixXd & B, Eigen::MatrixXd & C);
 void multplicacion_armadillo(arma::Mat<double> & A,arma::Mat<double>& B,arma::Mat<double> & C);
 void multiplicacion_blocking(const std::vector<double> a,const std::vector<double> b,std::vector<double> &c,int Nb);
 #endif
