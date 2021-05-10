cbaa4d815ff774dd4b08ae62d201d8f160463beb
JoseBernalRodriguez <62001295+JoseBernalRodriguez@users.noreply.github.com>
2021-05-10 17:16:41 -0500

    Update Performance_Matrix_Size.cpp

--- Performance_Matrix_Size.cpp
+++ Performance_Matrix_Size.cpp
@@ -10,6 +10,19 @@
 double desviacion_estandar(std::vector<double> & MFLOPS);	//Se inicializa el vector de desviación estándar
 void fill_random_vector(std::vector<double> & v);		//
 
+#define PAPI1 if((retval=PAPI_flops_rate(PAPI_FP_OPS,&ireal_time,&iproc_time,&iflpops,&imflops)) < PAPI_OK)\	
+			{\
+		 		printf("Could not initialise PAPI_flops \n");\
+		 		printf("Your platform may not support floating point operation event.\n");\
+	 	 		printf("retval: %d\n", retval);\
+		 		exit(1);\
+			}\
+			
+#define PAPI2 	if((retval=PAPI_flops_rate(PAPI_FP_OPS,&real_time, &proc_time, &flpops, &mflops))<PAPI_OK)\
+			{\
+	 	 		printf("retval: %d\n", retval);\
+	 			exit(1);\
+			}\
 
 int main(int argc,char**argv)
 {
@@ -34,70 +47,111 @@ int main(int argc,char**argv)
  	for(int jj=0;jj<15;jj++) 	//For cicle to variate the size of the matrix for 1 to 16384
 	{
        		int Nmax=std::pow(2,jj);
-		std::vector<double> a(Nmax*Nmax,0);
-		std::vector<double> b(Nmax*Nmax,0);
-		std::vector<double> c(Nmax*Nmax,0);
-		Eigen::MatrixXd AE = Eigen::MatrixXd::Random(Nmax,Nmax);
-		Eigen::MatrixXd BE = Eigen::MatrixXd::Random(Nmax,Nmax);	
-		Eigen::MatrixXd CE = Eigen::MatrixXd::Zero(Nmax, Nmax);
-		arma::mat AA(Nmax, Nmax, arma::fill::randu);
-		arma::mat BA(Nmax, Nmax, arma::fill::randu);
-		arma::mat CA(Nmax, Nmax); CA.zeros();
-		fill_random_vector(a);
-		fill_random_vector(b);
-	        //Nmax determinates the size of the matrix
+		
 		for(int ii=0; ii<cuentas;ii++)		//For cicle to realize each count
   		{
-		  	
+
      		 	//if(Nmax<Nb) exit(0);
-     		 	if((retval=PAPI_flops_rate(PAPI_FP_OPS,&ireal_time,&iproc_time,&iflpops,&imflops)) < PAPI_OK)		//This code checks if PAPI can be used in the CPU
-			{
-		 		printf("Could not initialise PAPI_flops \n");
-		 		printf("Your platform may not support floating point operation event.\n");
-	 	 		printf("retval: %d\n", retval);
-		 		exit(1);
-			}
+     		 	std::ofstream trash ("Delete_me_please.txt");
+			double aux_sum=0.0;
       		//Here William determinates the code to measure
 	 		if( William == 0 )	//Initialization and calculation of Eigen Multiplication
 	 		{
-        		
+        			Eigen::MatrixXd AE = Eigen::MatrixXd::Random(Nmax,Nmax);
+        			Eigen::MatrixXd BE = Eigen::MatrixXd::Random(Nmax,Nmax);	
+				Eigen::MatrixXd CE = Eigen::MatrixXd::Zero(Nmax, Nmax);
+				PAPI1
      				multiplicacion_eigen(AE,BE,CE);
-	 		}
+	 			PAPI2
+				aux_sum=CE.sum();
+      			        trash << aux_sum ;
+			}
 	 		if( William == 1 )		//Initialization and calculation of Armadillo Multiplication
 	 		{
+       				arma::mat AA(Nmax, Nmax, arma::fill::randu);
+        			arma::mat BA(Nmax, Nmax, arma::fill::randu);
+				arma::mat CA(Nmax, Nmax); CA.zeros();
+				PAPI1
          		 	multplicacion_armadillo(AA,BA,CA);
-	 		}
+	 			PAPI2
+				aux_sum =arma::accu(CA);		
+      			        trash << aux_sum ;
+			}
 			if( William == 2 )		//Initialization and calculation of Direct Multiplication
 			{
+				std::vector<double> a(Nmax*Nmax,0);
+        			std::vector<double> b(Nmax*Nmax,0);
+        			std::vector<double> c(Nmax*Nmax,0);
+        			fill_random_vector(a);
+        			fill_random_vector(b);
+				PAPI1
 				multiplicacion_directa(a,b,c);
+				PAPI2
+				for(auto & x: c)
+	    		   	{ aux_sum += x;}				    
+				trash << aux_sum ;     
 			}
 			if( William == 3 )		//Initialization and calculation of Blocking multiplication
 			{
+				std::vector<double> a(Nmax*Nmax,0);
+        			std::vector<double> b(Nmax*Nmax,0);
+        			std::vector<double> c(Nmax*Nmax,0);
+        			fill_random_vector(a);
+        			fill_random_vector(b);
+				PAPI1
 				multiplicacion_blocking(a,b,c,Nb);
+				PAPI2
+				for(auto & x: c)
+	    		   	{ aux_sum += x;}				    
+				trash << aux_sum ;     
 			}
          		if ( William == 4 )		//Initialization and calculation of Eigen Transposition
 	 		{
-     			 	transpuesta_eigen(AE,CE);
-	 		}
+			 	Eigen::MatrixXd AE = Eigen::MatrixXd::Random(Nmax,Nmax);
+				Eigen::MatrixXd AET = Eigen::MatrixXd::Zero(Nmax, Nmax);
+				PAPI1
+     			 	transpuesta_eigen(AE,AET);
+	 			PAPI2
+				aux_sum=AET.sum();
+      			        trash << aux_sum ;
+			}
 	 		if( William == 5 )		//Initialization and calculation of Armadillo Transposition
 	 		{
-			 	transpuesta_armadillo(AA,CA);
-	 		}
+				arma::mat AA(Nmax, Nmax, arma::fill::randu);
+				arma::mat AT(Nmax, Nmax, arma::fill::randu);
+				PAPI1
+			 	transpuesta_armadillo(AA,AT);
+	 			PAPI2
+				aux_sum =arma::accu(AT);		
+      			        trash << aux_sum ;
+			}
 			
 			if( William == 6 )		//Initialization and calculation of Direct Transposition
 			{
-				transpuesta_directa(a,c);
+				std::vector<double> a(Nmax*Nmax,0);
+				std::vector<double> at(Nmax*Nmax,0);
+				fill_random_vector(a);
+				PAPI1
+				transpuesta_directa(a,at);
+				PAPI2
+				for(auto & x: at)
+	    		   	{ aux_sum += x;}				    
+				trash << aux_sum ;     
 			}
 			if( William == 7 )		//Initialization and calculation of Blocking Transposition
 			{
-				transpuesta_blocking(a,c,Nb);
-			}
+				std::vector<double> a(Nmax*Nmax,0);
+				std::vector<double> b(Nmax*Nmax,0);
+				fill_random_vector(a);
+				PAPI1
+				transpuesta_blocking(a,b,Nb);
+				PAPI2
+				for(auto & x: b)
+	    		   	{ aux_sum += x;}				    
+				trash << aux_sum ;     
+			}	
 			
-      			if((retval=PAPI_flops_rate(PAPI_FP_OPS,&real_time, &proc_time, &flpops, &mflops))<PAPI_OK)
-			{
-	 	 		printf("retval: %d\n", retval);
-	 			exit(1);
-			}
+      			
 			//Vectors of PAPI are filled for the code calculated by william
       			MFLOPS[0]+=mflops/cuentas;
       			REAL_TIME[0]+=real_time/cuentas;
@@ -105,33 +159,8 @@ int main(int argc,char**argv)
       			MFLOPS[ii+1]=mflops;
       			REAL_TIME[ii+1]=real_time;
       			PROC_TIME[ii+1]=proc_time;
-			double aux_sum=0.0;
-			//Some results of the code are printed and saved in a .txt file to forze the CPU to calculate them
-      			std::ofstream trash ("Delete_me_please.txt");
-			if(William==0 || William==4)
-			  {
-			        aux_sum=CE.sum();
-      			        trash << aux_sum ;
-	    
-      			      
-			  }
-			if(William==1 || William==5)
-			  {
-			        aux_sum =arma::accu(CA);		
-      			        trash << aux_sum ;
 			
-      			      
-			  }
-			if(William==2 || William==3 || William==6 || William==7)
-			  {
-		           	for(auto & x: c)
-	    		   	{	
-        		      	     aux_sum += x;
-				     
-			  	 }
-				trash << aux_sum ;     			         
-			  }
-			  trash.close();
+ 			trash.close();			
 	  }
 	 //peak 18.64 Gflops
 	 //The results that are gonna be used are printed 
