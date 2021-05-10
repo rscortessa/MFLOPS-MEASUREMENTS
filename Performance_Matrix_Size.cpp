619d8700bd4d24b9ff5daa33a0fe5f782cfb6da6
Federico-Aranguren <79715183+Federico-Aranguren@users.noreply.github.com>
2021-05-09 22:37:39 -0500

    Comments were added

--- Performance_Matrix_Size.cpp
+++ Performance_Matrix_Size.cpp
@@ -1,75 +1,76 @@
-#include <stdio.h>
+#include <stdio.h>									
 #include <stdlib.h>
 #include <papi.h>
 #include <random>
 #include <iostream>
 #include <fstream>
 #include <cmath>
-#include "Multiplicaciones.h"
-#include "Transpuestas.h"
-double desviacion_estandar(std::vector<double> & MFLOPS);
-void fill_random_vector(std::vector<double> & v);
+#include "Multiplicaciones.h"				//Se incluye el código de Multiplicaciones.cpp
+#include "Transpuestas.h"				//Se incluye el código de transpuestas.cpp
+double desviacion_estandar(std::vector<double> & MFLOPS);	//Se inicializa el vector de desviación estándar
+void fill_random_vector(std::vector<double> & v);		//
 
 
 int main(int argc,char**argv)
 {
 	std::cout.precision(7); std::cout.setf(std::ios::scientific);
-	int Nb=8 ; //optim size blocking
-	int William = std::atoi(argv[1]); //william determines the code to measure
-	float real_time, proc_time,mflops;
-  	long long flpops;
-  	float ireal_time, iproc_time, imflops;
-  	long long iflpops;
-  	int retval;
-  	int cuentas= std::atoi(argv[2]);
-	//Inicializar las variables auxiliares
+	int Nb=8 ; //optimum size for blocking
+	int William = std::atoi(argv[1]); //william determinates the code to measure
+	//Variables for PAPI are initialized
+	float real_time, proc_time,mflops;	
+  	long long flpops;			
+  	float ireal_time, iproc_time, imflops;	
+  	long long iflpops;			
+  	int retval;				
+  	int cuentas= std::atoi(argv[2]);	//Determinates the number of counts made by the program
+	//Aux variables are initialized
  	std::vector<double> MFLOPS(cuentas+1,0);
  	std::vector<double> REAL_TIME(cuentas+1,0);
  	std::vector<double> PROC_TIME(cuentas+1,0);
 
-
-	 std::cout<<"MFLOPS"<<" \t "<<"MFLOPS%"<<" \t "<<"REAL_TIME"<<" \t "<<"REAL_TIME%"<<" \t "<<"PROC_TIME"<<" \t "<<"PROC_TIME%"<<" \t "<<"Nmax"<<std::endl;
- 	for(int jj=0;jj<15;jj++)
+	//Variables used to measure the development of the code with PAPI are printed
+	std::cout<<"MFLOPS"<<" \t "<<"MFLOPS%"<<" \t "<<"REAL_TIME"<<" \t "<<"REAL_TIME%"<<" \t "<<"PROC_TIME"<<" \t "<<"PROC_TIME%"<<" \t "<<"Nmax"<<std::endl;
+ 	for(int jj=0;jj<15;jj++) 	//For cicle to variate the size of the matrix for 1 to 16384
 	{
-       		int Nmax=std::pow(2,jj);
-		for(int ii=0; ii<cuentas;ii++)
+       		int Nmax=std::pow(2,jj);	//Nmax determinates the size of the matrix
+		for(int ii=0; ii<cuentas;ii++)		//For cicle to realize each count
   		{
 
      		 	//if(Nmax<Nb) exit(0);
-     		 	if((retval=PAPI_flops_rate(PAPI_FP_OPS,&ireal_time,&iproc_time,&iflpops,&imflops)) < PAPI_OK)
+     		 	if((retval=PAPI_flops_rate(PAPI_FP_OPS,&ireal_time,&iproc_time,&iflpops,&imflops)) < PAPI_OK)		//This code checks if PAPI can be used in the CPU
 			{
 		 		printf("Could not initialise PAPI_flops \n");
 		 		printf("Your platform may not support floating point operation event.\n");
 	 	 		printf("retval: %d\n", retval);
 		 		exit(1);
 			}
-      		//Se coloca el código a medir...
-	 		if( William == 0 )
+      		//Here William determinates the code to measure
+	 		if( William == 0 )	//Initialization and calculation of Eigen Multiplication
 	 		{
         			Eigen::MatrixXd AE = Eigen::MatrixXd::Random(Nmax,Nmax);
         			Eigen::MatrixXd BE = Eigen::MatrixXd::Random(Nmax,Nmax);	
 				Eigen::MatrixXd CE = Eigen::MatrixXd::Zero(Nmax, Nmax);
      				multiplicacion_eigen(AE,BE,CE);
 	 		}
-	 		if( William == 1 )
+	 		if( William == 1 )		//Initialization and calculation of Armadillo Multiplication
 	 		{
        				arma::mat AA(Nmax, Nmax, arma::fill::randu);
         			arma::mat BA(Nmax, Nmax, arma::fill::randu);
 				arma::mat CA(Nmax, Nmax); CA.zeros();
          		 	multplicacion_armadillo(AA,BA,CA);
 	 		}
-         		if( William == 2 )
+         		( William == 2 )|		//Initialization and calculation of Eigen Transposition
 	 		{
 			 	Eigen::MatrixXd AE = Eigen::MatrixXd::Random(Nmax,Nmax);
 				Eigen::MatrixXd AET = Eigen::MatrixXd::Zero(Nmax, Nmax);
      			 	transpuesta_eigen(AE,AET);
 	 		}
-	 		if( William == 3 )
+	 		if( William == 3 )		//Initialization and calculation of Armadillo Transposition
 	 		{
 				arma::mat AA(Nmax, Nmax, arma::fill::randu);
 			 	transpuesta_armadillo(AA);
 	 		}
-			if( William == 4 )
+			if( William == 4 )		//Initialization and calculation of Blocking multiplication
 			{
 				std::vector<double> a(Nmax*Nmax,0);
         			std::vector<double> b(Nmax*Nmax,0);
@@ -78,7 +79,7 @@ int main(int argc,char**argv)
         			fill_random_vector(b);
 				multiplicacion_blocking(a,b,c,Nb);
 			}
-			if( William == 5 )
+			if( William == 5 )		//Initialization and calculation of Blocking Transposition
 			{
 				std::vector<double> a(Nmax*Nmax,0);
 				std::vector<double> b(Nmax*Nmax,0);
@@ -86,7 +87,7 @@ int main(int argc,char**argv)
 				fill_random_vector(b);
 				transpuesta_blocking(a,b,Nb);
 			}
-			if( William == 6 )
+			if( William == 6 )		//Initialization and calculation of Direct Multiplication
 			{
 				std::vector<double> a(Nmax*Nmax,0);
         			std::vector<double> b(Nmax*Nmax,0);
@@ -95,7 +96,7 @@ int main(int argc,char**argv)
         			fill_random_vector(b);
 				multiplicacion_directa(a,b,c);
 			}
-			if( William == 7 )
+			if( William == 7 )		//Initialization and calculation of Direct Transposition
 			{
 				std::vector<double> a(Nmax*Nmax,0);
 				fill_random_vector(a);
@@ -107,6 +108,7 @@ int main(int argc,char**argv)
 	 	 		printf("retval: %d\n", retval);
 	 			exit(1);
 			}
+			//Vectors of PAPI are filled for the code calculated by william
       			MFLOPS[0]+=mflops/cuentas;
       			REAL_TIME[0]+=real_time/cuentas;
       			PROC_TIME[0]+=proc_time/cuentas;
@@ -114,10 +116,9 @@ int main(int argc,char**argv)
       			REAL_TIME[ii+1]=real_time;
       			PROC_TIME[ii+1]=proc_time;
 
-      			// En este espacio se imprime algún resultado del código
+      			//Some results of the code are printed and saved in a .txt file to forze the CPU to calculate them
       			std::ofstream trash ("Delete_me_please.txt");
       			double aux_sum=0.0;
-      			// Se guarda en un archivo de texto dado que no se quiere que aparezca en al ejecución
 			std::vector<double> c(Nmax*Nmax,0);
       			for(auto x: c)
       			{	
@@ -127,6 +128,7 @@ int main(int argc,char**argv)
       			trash.close();
 		}
 	 //peak 18.64 Gflops
+	 //The results that are gonna be used are printed 
 	 std::cout<<MFLOPS[0]/(18640)<<"\t "<<desviacion_estandar(MFLOPS)/(18640)<<"\t "<<REAL_TIME[0]<<"\t "<<desviacion_estandar(REAL_TIME)<<"\t "<<PROC_TIME[0]<<"\t"<<desviacion_estandar(PROC_TIME)<<"\t"<<Nmax<<std::endl;
 	 REAL_TIME[0]=0;
  	 MFLOPS[0]=0;
@@ -136,20 +138,20 @@ int main(int argc,char**argv)
 }
 
 
-double desviacion_estandar(std::vector<double> & MFLOPS)
+double desviacion_estandar(std::vector<double> & MFLOPS)	//The function desviacion_estandar is defined
 {
   double sigma=0;
   double N=MFLOPS.size();
   for(int i=1;i<N;i++)
     {
-      sigma+=std::pow((MFLOPS[i]-MFLOPS[0]),2);
+      sigma+=std::pow((MFLOPS[i]-MFLOPS[0]),2);		//The square of the standard deviation is calculated
     }
-  sigma=std::sqrt(sigma/N);
+  sigma=std::sqrt(sigma/N);		//The standard deviation is finally calculated
   return sigma;
 }
 
 
-void fill_random_vector(std::vector<double> & v)
+void fill_random_vector(std::vector<double> & v)		//This vector is used to fill the Matrixes used in direct/blocking multiplication and transposition
 {
     std::mt19937 gen(1);
     std::uniform_real_distribution<> dist(-1.0,1.0);
